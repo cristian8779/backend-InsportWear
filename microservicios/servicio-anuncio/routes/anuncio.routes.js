9dbd32e01e47a1ae82bc81f2db4a02570bd68674
Cristian <cr6145396@gmail.com>
2025-07-07 16:49:13 -0500

    feat(anuncio): se actualiza la ruta de anuncios y se añade .gitignore

--- microservicios/servicio-anuncio/routes/anuncio.routes.js
+++ microservicios/servicio-anuncio/routes/anuncio.routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const ctrl = require("../controllers/anuncioController");
+const upload = require('../middlewares/upload');
+
 
 router.get("/activos", ctrl.obtenerActivos);
-router.post("/", auth, ctrl.crearAnuncio);
+router.post("/", auth, upload.single("imagen"), ctrl.crearAnuncio);
 router.delete("/:id", auth, ctrl.eliminarAnuncio);
 
 module.exports = router;
